99d054b97e0a94e7504c69e6c3abff91d0534407
Mikhail Bezhaneishvili <mike215misha@gmail.com>
2023-11-20 22:35:10 +0300

    add solution java syntax task0201

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int intValue  = 12, numericValue = 14, size = 43,  number = 5;
+
+    }
+}
