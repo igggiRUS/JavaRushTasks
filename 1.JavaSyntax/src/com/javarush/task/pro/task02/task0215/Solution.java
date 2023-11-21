e21dd27fc14d0b40272898beb66e592f1572a159
Mikhail Bezhaneishvili <mike215misha@gmail.com>
2023-11-21 23:43:46 +0300

    add solution java syntax task0215

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0215/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task02.task0215;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+int one = scanner.nextInt();
+int two = scanner.nextInt();
+int three = scanner.nextInt();
+        System.out.println((one + two + three) / 3);
+    }
+}
