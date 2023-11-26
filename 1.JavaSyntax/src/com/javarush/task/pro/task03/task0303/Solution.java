58cff72a375e07cf43ce8e298fc71ce0121ed7da
Mikhail Bezhaneishvili <mike215misha@gmail.com>
2023-11-26 13:46:43 +0300

    add solution java syntax task0303

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0303/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task03.task0303;
+
+import java.util.Scanner;
+
+/* 
+Школа или институт
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        if ((age < 18) && ((age >= 6) || !(age < 6)))
+                System.out.println("нужно ходить в школу");
+        else if (age < 6) {
+
+        }
+        else
+            System.out.println("пора в институт");
+    }
+}
