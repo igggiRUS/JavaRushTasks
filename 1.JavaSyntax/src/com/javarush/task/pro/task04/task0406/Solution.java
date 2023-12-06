b235bdd556562bb3322f87b0880a0698dea64f5f
Mikhail Bezhaneishvili <mike215misha@gmail.com>
2023-12-06 22:28:28 +0300

    add solution java syntax task0406

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0406/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0406/Solution.java
@@ -2,23 +2,17 @@ package com.javarush.task.pro.task04.task0406;
 
 import java.util.Scanner;
 
-/* 
-Показываем, что получаем
-*/
-
 public class Solution {
     public static void main(String[] args) {
-//        System.out.println("вводи: ");
         Scanner scanner = new Scanner(System.in);
         String isEnough = "enough";
         while (true) {
             String string = scanner.nextLine();
-            if ((isEnough.equals(string))) {
-                System.out.println(isEnough);
+            if (!(isEnough.equals(string))) {
+                System.out.println(string);
+            } else {
                 break;
             }
-
         }
     }
 }
-
