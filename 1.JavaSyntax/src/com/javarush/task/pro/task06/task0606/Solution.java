2786364e02da882b2bf5adac16a831e1149d65e2
Mikhail Bezhaneishvili <mike215misha@gmail.com>
2023-12-16 00:03:52 +0300

    add solution java syntax task0606

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.pro.task06.task0606;
+
+/*
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod() {
+
+    }
+
+    public static void universalMethod(int brave, int rangeOfFire, boolean fortune, String command, String battleCry, String medicine, boolean learningAbility, double shootingAccuracy, double fatigue) {
+    }
+
+    public static void universalMethod(int rangeOfFire, boolean fortune, String command, String battleCry, String medicine, boolean learningAbility, double shootingAccuracy, double fatigue) {
+    }
+
+    public static void universalMethod(boolean fortune, String command, String battleCry, String medicine, boolean learningAbility, double shootingAccuracy, double fatigue) {
+    }
+
+    public static void universalMethod(String command, String battleCry, String medicine, boolean learningAbility, double shootingAccuracy, double fatigue) {
+    }
+
+    public static void universalMethod(String battleCry, String medicine, boolean learningAbility, double shootingAccuracy, double fatigue) {
+    }
+
+    public static void universalMethod(String medicine, boolean learningAbility, double shootingAccuracy, double fatigue) {
+    }
+
+    public static void universalMethod(boolean learningAbility, double shootingAccuracy, double fatigue) {
+    }
+
+    public static void universalMethod(double shootingAccuracy, double fatigue) {
+    }
+
+    public static void universalMethod(double fatigue) {
+    }
+}
+
