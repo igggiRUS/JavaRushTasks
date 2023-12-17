4fabf667cbd4062b0152122f1b1563b5de702711
Mikhail Bezhaneishvili <mike215misha@gmail.com>
2023-12-18 00:15:21 +0300

    add solution java syntax task0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    int number = 1;
+    char a = ' ';
+    String string = "asdf";
+    float aFloat = 23.02F;
+    double aDouble = 32.4;
+    boolean aBoolean = true;
+    long aLong = 32;
+    byte aByte = 12;
+    short aShort = 32;
+
+
+}
