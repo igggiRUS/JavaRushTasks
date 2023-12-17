b2ddc9a8c16d153a533845f742ae67c14f4902f5
Mikhail Bezhaneishvili <mike215misha@gmail.com>
2023-12-18 00:16:06 +0300

    add solution java syntax task0703

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    byte maxByte = Byte.MIN_VALUE;
+    short maxShort = Short.MIN_VALUE;
+    int maxIng = Integer.MIN_VALUE;
+    long maxLong = Long.MIN_VALUE;
+    float maxFloat = Float.MIN_VALUE;
+    double maxDouble = Double.MIN_VALUE;
+}
