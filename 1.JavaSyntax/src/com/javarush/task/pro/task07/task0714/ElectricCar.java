03d3275a7c6be6a73076e1cf2c1d5b4abf1a0f87
Mikhail Bezhaneishvili <mike215misha@gmail.com>
2023-12-17 04:36:50 +0300

    add solution java syntax task0802

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0714/ElectricCar.java
@@ -0,0 +1,8 @@
+package com.javarush.task.pro.task07.task0714;
+
+public class ElectricCar extends Car{
+    double maxSpeed;
+    int wheelCount;
+    double weight;
+    int electricEnginePower;
+}
