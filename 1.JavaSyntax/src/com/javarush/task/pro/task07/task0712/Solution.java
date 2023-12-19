4b1dad4190e4a5833461356bd1d8bd592749db21
Mikhail Bezhaneishvili <mike215misha@gmail.com>
2023-12-19 07:10:07 +0300

    add solution java syntax task0712

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String string01 = new String("01 строка");
+        String string02 = new String("02 строка");
+        String string03 = new String("03 строка");
+        String string04 = new String("04 строка");
+        String string05 = new String("05 строка");
+        String string06 = new String("06 строка");
+        String string07 = new String("07 строка");
+        String string08 = new String("08 строка");
+        String string09 = new String("09 строка");
+        String string10 = new String("10 строка");
+
+        int[] ins01 = new int[1];
+        int[] ins02 = new int[2];
+        int[] ins03 = new int[3];
+        int[] ins04 = new int[4];
+        int[] ins05 = new int[5];
+
+        Scanner scanner01 = new Scanner(System.in);
+        Scanner scanner02 = new Scanner(System.in);
+
+
+    }
+}
