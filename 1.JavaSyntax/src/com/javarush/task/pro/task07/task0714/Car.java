bdad3026f143146dfc79359d460a8c177bb4ff5f
Mikhail Bezhaneishvili <mike215misha@gmail.com>
2023-12-20 00:19:42 +0300

    add solution java syntax task0714

--- 1.JavaSyntax/src/com/javarush/task/pro/task07/task0714/Car.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0714/Car.java
@@ -1,7 +1,10 @@
 package com.javarush.task.pro.task07.task0714;
 
-public class Car extends Vehicle {
-    double maxSpeed;
+/* 
+Наследование переменных
+*/
+
+public class Car extends Vehicle{
     int wheelCount;
     double weight;
 }
--- 1.JavaSyntax/src/com/javarush/task/pro/task07/task0714/ElectricCar.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0714/ElectricCar.java
@@ -1,8 +1,9 @@
 package com.javarush.task.pro.task07.task0714;
 
-public class ElectricCar extends Car{
-    double maxSpeed;
-    int wheelCount;
-    double weight;
+/* 
+Наследование переменных
+*/
+
+public class ElectricCar extends Car {
     int electricEnginePower;
 }
