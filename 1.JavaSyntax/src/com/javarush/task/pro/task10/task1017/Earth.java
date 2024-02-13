c470656e3b8d6aec8c5286fbf8caabf703a4e57f
Mikhail Bezhaneishvili <mike215misha@gmail.com>
2024-02-13 21:35:34 +0300

    add solution java syntax task1017

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1017/Earth.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task10.task1017;
+
+/* 
+Создание материков
+*/
+
+public class Earth {
+    public static void main(String[] args) {
+       new Africa(222);
+       new Antarctica(232);
+       new Australia(233);
+       new Eurasia(234);
+       new NorthAmerica(235);
+       new SouthAmerica(236);
+
+
+    }
+}
