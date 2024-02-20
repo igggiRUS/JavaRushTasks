f58d936958bf601d51f643dbc2565e21dc1ad50a
Mikhail Bezhaneishvili <mike215misha@gmail.com>
2024-02-20 20:55:16 +0300

    add solution java syntax task1104

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1104/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task11.task1104;
+
+/* 
+Конвертер строки
+*/
+
+import static java.lang.Double.parseDouble;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+        double aDouble;
+        String string = "12.84";
+        aDouble = Double.parseDouble(string);
+        System.out.println(Math.round(aDouble));
+    }
+
+
+}
