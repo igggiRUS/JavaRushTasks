683c9c5f5013ee018003a5a73e4e19789726512e
Mikhail Bezhaneishvili <mike215misha@gmail.com>
2024-02-21 22:38:37 +0300

    add solution java syntax task1316

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1316/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task13.task1316;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        JavarushQuest[] quests = JavarushQuest.values();
+
+
+        for (JavarushQuest quest :
+                JavarushQuest.values()) {
+            System.out.println(quest.ordinal());
+        }
+    }
+}
