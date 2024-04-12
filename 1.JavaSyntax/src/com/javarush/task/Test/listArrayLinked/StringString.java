574eb2c133323712bc7b5534d5797897f2a6e50b
Mikhail Bezhaneishvili <mike215misha@gmail.com>
2024-04-12 21:15:11 +0300

    add solution java syntax 1314 StrintString.java

---
+++ 1.JavaSyntax/src/com/javarush/task/Test/listArrayLinked/StringString.java
@@ -0,0 +1,11 @@
+package com.javarush.task.Test.listArrayLinked;
+
+import java.util.ArrayList;
+
+public class StringString {
+    //             коллекция интерфейсом Лист Sting,     массив String
+    //
+    public static void addAll(ArrayList<String> list, String... strings){
+
+    }
+}
